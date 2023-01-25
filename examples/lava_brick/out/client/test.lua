bcab814b36eb10aae7e86fca22ffd54c71402110
AlphaRunic <roleyspersonal@gmail.com>
2023-01-24 21:14:58 -0800

    Fix function call precedence

--- examples/lava_brick/out/client/test.lua
+++ examples/lava_brick/out/client/test.lua
@@ -1,10 +1,10 @@
 local ruby = require(game.ReplicatedStorage.RubyLib)
 
 local lava = script.Parent
-return (type(lava.Touched) == "function" and lava:Touched() or lava.Touched).Connect(function(hit)
+return (type(lava.Touched) == "function" and lava:Touched() or lava.Touched):Connect(function(hit)
     local parent = (type(hit.Parent) == "function" and hit:Parent() or hit.Parent)
-    local humanoid = parent.FindFirstChildOfClass"Humanoid"
+    local humanoid = parent:FindFirstChildOfClass("Humanoid")
     if humanoid then    
-        humanoid.TakeDamage(type(humanoid.Health) == "function" and humanoid:Health() or humanoid.Health)
+        humanoid:TakeDamage((type(humanoid.Health) == "function" and humanoid:Health() or humanoid.Health))
     end
 end)
