419371ca836e1375456b7e4498a6b5841e15a0b9
r-unic <roleyspersonal@gmail.com>
2023-01-27 10:33:58 -0800

    Fix `== nil` suffix

--- examples/lava_brick/out/client/test.lua
+++ examples/lava_brick/out/client/test.lua
@@ -1,4 +1,4 @@
-local ruby = require(game.ReplicatedStorage.RubyLib)
+local ruby = require(game.ReplicatedStorage.Ruby.Runtime)
 
 local collection = game:GetService("CollectionService")
 local lava_bricks = collection:GetTagged("Lava")
@@ -6,7 +6,7 @@ for lava in ruby.list(lava_bricks) do
     (type(lava.Touched) == "function" and lava:Touched() or lava.Touched):Connect(function(hit)    
         local parent = (type(hit.Parent) == "function" and hit:Parent() or hit.Parent)
         local humanoid = parent:FindFirstChildOfClass("Humanoid")
-        if humanoid == nil == nil then        
+        if humanoid == nil then        
             humanoid:TakeDamage((type(humanoid.Health) == "function" and humanoid:Health() or humanoid.Health))
         end
     end)
