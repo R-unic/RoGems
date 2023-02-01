ddcce523b2dbf3be367575e08c56279cc702d142
r-unic <roleyspersonal@gmail.com>
2023-01-31 20:19:16 -0800

    Remove require in RoGems.rb

--- lib/RoGems.rb
+++ lib/RoGems.rb
@@ -1,7 +1,6 @@
 $:.unshift File.join(File.dirname(__FILE__), "rogems")
 require "yaml"
 require "CLI"
-require "rogems/compiler_types"
 
 module RoGems
     CONFIG_PATH = File.join(File.dirname(__FILE__), "../rogems.config.yml")
