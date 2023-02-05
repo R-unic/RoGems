0b4ea4a6dc0773fb6f138e9e7194d27857f80c40
r-unic <roleyspersonal@gmail.com>
2023-02-05 01:58:12 -0800

    Restrict TS compilation to only TSX

--- examples/using_roact/out/client/roact/test.lua
+++ examples/using_roact/out/client/roact/test.lua
@@ -1,4 +1,4 @@
--- Compiled with roblox-ts v2.0.4
+-- Compiled with roblox-ts v2.1.0
 local TS = require(game:GetService("ReplicatedStorage"):WaitForChild("TS_Include"):WaitForChild("RuntimeLib"))
 local Roact = TS.import(script, game:GetService("ReplicatedStorage"), "TS_Include", "node_modules", "@rbxts", "roact", "src")
 local element = (Roact.createElement("Frame", {
